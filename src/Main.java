7905d38f4c170d6bcb0d5dc280be3d3fdf376ef9
saleem shaik <saleemshaik0226@gmail.com>
2022-03-08 16:07:17 -0500

    made changes in the main file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("new git code is added");
         Wall wall1 = new Wall("east");
         Wall wall2 = new Wall("west");
         Wall wall3 = new Wall("north");
